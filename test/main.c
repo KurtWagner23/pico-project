0a9423d5cd7fc39211c7d707fdad9cbf4275903c
KurtWagner23 <philip.boehringer@web.de>
2023-12-13 17:17:12 +0100

    first commit

---
+++ test/main.c
@@ -0,0 +1,19 @@
+#include <stdarg.h>
+#include <setjmp.h>
+#include <stddef.h>
+#include <cmocka.h>
+
+static void test(void **state)
+{
+    assert_int_equal(2, 2);
+}
+
+int main()
+{
+    const struct CMUnitTest tests[] =
+        {
+            cmocka_unit_test(test),
+        };
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
